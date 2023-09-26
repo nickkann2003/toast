9a28664bbfe0ca32baf5407e8c2c45c071644584
Nick Kannenberg <nrk5987@rit.edu>
2023-09-26 12:04:29 -0400

    Fixed inventory script

--- Toast/Assets/Scripts/Inventory.cs
+++ Toast/Assets/Scripts/Inventory.cs
@@ -2,15 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Inventory<GameObject>
+/*
+ * Inventory Class
+ * ----------------------
+ * Handles all item storage
+ */
+public class Inventory
 {
+    // Variables ----------------------------------------------
     private List<GameObject> inventoryItems;
 
-    public Inventory<GameObject>(){
-        inventoryItems = new List<GameObject>;
+    // Constructors -------------------------------------------
+    public Inventory(){
+        inventoryItems = new List<GameObject>();
     }
 
-    public Inventory<GameObject>(List<GameObject> items){
+    public Inventory(List<GameObject> items){
       inventoryItems = items;
+    }
+
+    // Functions ----------------------------------------------
+    public void addItem(GameObject item)
+    {
+        if (!inventoryItems.Contains(item))
+        {
+            inventoryItems.Add(item);
+        }
+        else
+        {
+            return;
+        }
+    }
+    public void removeItem(GameObject item)
+    {
+        if(inventoryItems.Contains(item))
+        {
+            inventoryItems.Remove(item);
+        }
+        else
+        {
+            return;
+        }
     }
 }
