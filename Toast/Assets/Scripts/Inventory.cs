6e20627b8f8e20a62f0da8a859d1d6b5d3afbb2f
Nick Kannenberg <nrk5987@rit.edu>
2023-09-26 16:39:11 -0400

    Added missing sprite, added to inverntoy

--- Toast/Assets/Scripts/Inventory.cs
+++ Toast/Assets/Scripts/Inventory.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 /*
@@ -10,38 +11,92 @@ using UnityEngine;
 public class Inventory
 {
     // Variables ----------------------------------------------
-    private List<GameObject> inventoryItems;
+    private InventoryObject[] inventoryItems;
 
     // Constructors -------------------------------------------
     public Inventory(){
-        inventoryItems = new List<GameObject>();
+        inventoryItems = new InventoryObject[10];
     }
 
     public Inventory(List<GameObject> items){
-      inventoryItems = items;
+        if(items.Count > 10)
+        {
+            inventoryItems = new InventoryObject[items.Count];
+        }
+        else
+        {
+            inventoryItems = new InventoryObject[10];
+        }
+
+        for(int i = 0; i < items.Count-1; i++)
+        {
+            inventoryItems[i] = new InventoryObject(items[i]);
+        }
     }
 
     // Functions ----------------------------------------------
     public void addItem(GameObject item)
     {
-        if (!inventoryItems.Contains(item))
+        for(int i = 0;i < inventoryItems.Length;i++)
         {
-            inventoryItems.Add(item);
-        }
-        else
-        {
-            return;
+            if (inventoryItems[i] == null)
+            {
+                inventoryItems[i] = new InventoryObject(item);
+            }
         }
     }
-    public void removeItem(GameObject item)
+    public GameObject removeItem(int position)
     {
-        if(inventoryItems.Contains(item))
+        if (inventoryItems[position] == null)
         {
-            inventoryItems.Remove(item);
+            GameObject removedObject = inventoryItems[position].GameObject;
+            inventoryItems[position] = null;
+            return removedObject;
         }
-        else
-        {
-            return;
-        }
+        return null;
+    }
+
+}
+
+class InventoryObject
+{
+    // Variables ----------------------------------------------
+    private GameObject gameObject;
+    protected bool selectable;
+    protected Sprite icon;
+
+    public GameObject GameObject { get => gameObject; }
+
+    // Constructors -------------------------------------------
+    public InventoryObject(GameObject gameObject)
+    {
+        this.gameObject = gameObject;
+        selectable = true;
+    }
+
+    public InventoryObject(GameObject gameObject, bool selectable)
+    {
+        this.gameObject = gameObject;
+        this.selectable = selectable;
+    }
+
+    public InventoryObject(GameObject gameObject, Sprite icon)
+    {
+        this.gameObject = gameObject;
+        this.icon = icon;
+        selectable = true;
+    }
+
+    public InventoryObject(GameObject gameObject, bool selectable, Sprite icon)
+    {
+        this.gameObject = gameObject;
+        this.icon = icon;
+        this.selectable = selectable;
+    }
+
+    // Functions ----------------------------------------------
+    public void setSelectable(bool selectable)
+    {
+        this.selectable = selectable;
     }
 }
