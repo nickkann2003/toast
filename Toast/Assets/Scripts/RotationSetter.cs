03a740a6511d7d8081a57e98441f951a8bfb6da1
Nick Kannenberg (RIT Student) <nrk5987@rit.edu>
2023-10-05 15:18:07 -0400

    Added toast rotator

---
+++ Toast/Assets/Scripts/RotationSetter.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationSetter : MonoBehaviour
+{
+    public Vector3 targetRotationEuler = new Vector3(0, 90, 0);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void setRotate(Collider other)
+    {
+        Rigidbody otherRigidbody = other.GetComponent<Rigidbody>();
+        if(otherRigidbody != null)
+        {
+            // Get the transform of the GameObject.
+            Transform objectTransform = other.transform;
+            //otherRigidbody.angularVelocity = Vector3.zero;
+            
+            // Convert Euler angles to a Quaternion for rotation.
+            Quaternion targetRotation = Quaternion.Euler(targetRotationEuler);
+
+            if(objectTransform.rotation != targetRotation)
+            {
+                // Calculate the quaternion delta.
+                Quaternion deltaRotation = targetRotation * Quaternion.Inverse(objectTransform.rotation);
+
+                // Convert the delta rotation to an angle-axis representation.
+                float angle;
+                Vector3 axis;
+                deltaRotation.ToAngleAxis(out angle, out axis);
+
+                // Ensure the angle is in the range [-180, 180] degrees.
+                if (angle > 180f)
+                {
+                    angle -= 360f;
+                }
+
+                // Calculate the angular velocity as the axis of rotation scaled by the angle.
+                Vector3 angularVelocity = axis * (angle * Mathf.Deg2Rad);
+                angularVelocity = angularVelocity.normalized * 3;
+
+                if(otherRigidbody.angularVelocity != angularVelocity)
+                {
+                    otherRigidbody.angularVelocity += (angularVelocity - otherRigidbody.angularVelocity)/20f;
+                }
+
+                //otherRigidbody.angularVelocity = angularVelocity;
+
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        setRotate(other);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        setRotate(other);
+    }
+}
