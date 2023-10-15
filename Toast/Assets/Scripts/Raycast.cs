573045ca4f0bbd83f8f7e2557c1d42db35e827bc
Zachary Hooper <the1errandboy@gmail.com>
2023-10-14 22:37:50 -0400

    added a line on drag

--- Toast/Assets/Scripts/Raycast.cs
+++ Toast/Assets/Scripts/Raycast.cs
@@ -6,17 +6,20 @@ using UnityEngine.UI;
 
 public class Raycast : MonoBehaviour
 {
-    public Camera targetCamera;
+    Camera targetCamera;
 
-    // debug canvas stuff
-    public GameObject canvasGO;
-    public GameObject infoText;
+    GameObject prevGO;
+    GameObject hitGO;
 
-    private GameObject prevGO;
-    private GameObject hitGO;
+    GameObject selectGO;
+
+    public GameObject linePrefab;
+
+    GameObject line;
+    bool dragging;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         prevGO = null;
         hitGO = null;
@@ -42,9 +45,36 @@ public class Raycast : MonoBehaviour
             {
                 print($"Object: \"{hitGO.name}\"");
             }
+            
+            if (Input.GetMouseButtonDown(0))
+            {
+                line = Instantiate(linePrefab);
+                print($"Object: \"{hitGO.name}\"");
+                selectGO = hitGO;
+                print(hit.point);
+                line.GetComponent<LineController>().SetAnchor(selectGO.transform);
+                dragging = true;
+
+
+                //GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                //cube.transform.position = hit.point;
+                //hitGO.transform.parent = cube.transform;
+            }
+            if (Input.GetMouseButtonUp(0))
+            {
+                dragging = false;
+                Destroy(line);
+                line = null;
+            }
+
+            if (dragging)
+            {
+                //line.SetMouse(new Vector3());
+            }
 
             // draw ray at hit point
-            Debug.DrawRay(ray.origin, transform.InverseTransformPoint(hit.point), Color.yellow);
+            Debug.DrawLine(ray.origin, hit.point, Color.yellow);
+            //Debug.DrawRay(hit.point, Vector3.Reflect(transform.InverseTransformPoint(hit.point), hit.normal), Color.blue);
         }
     }
 }
