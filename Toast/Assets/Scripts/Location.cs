da65e8aaafdf776a778ec5efd9000dcf5323bfe6
Nick Kannenberg <nrk5987@rit.edu>
2023-10-24 12:14:37 -0400

    Finished incorporating new models, adjusted scene lighting to better show off new fridge light

--- Toast/Assets/Scripts/Location.cs
+++ Toast/Assets/Scripts/Location.cs
@@ -22,5 +22,10 @@ public class Location : Station
         }
     }
 
-   
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(cameraPos, 0.5f);
+    }
+
 }
