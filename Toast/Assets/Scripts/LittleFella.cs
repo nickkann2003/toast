1b0143f9e57443e61dc7af7654af57c90a998d21
mdt4624 <mdt4624@rit.edu>
2023-12-03 16:31:14 -0500

    Added script and objects for monster interaction

---
+++ Toast/Assets/Scripts/LittleFella.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LittleFella : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 dragGrabPos;
+    [SerializeField]
+    Vector3 dragHomePos;
+    [SerializeField]
+    GameObject edibleObject;
+    [SerializeField]
+    GameObject grabHand;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
