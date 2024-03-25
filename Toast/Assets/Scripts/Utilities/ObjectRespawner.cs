e4b987c0074c9048d09c5bd6778307a0048cc7c7
Nick Kannenberg <nick.kann2003@gmail.com>
2024-03-25 13:28:22 -0400

    Upgraded object respawner

--- Toast/Assets/Scripts/Utilities/ObjectRespawner.cs
+++ Toast/Assets/Scripts/Utilities/ObjectRespawner.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.Contracts;
@@ -5,29 +6,76 @@ using UnityEngine;
 
 public class ObjectRespawner : MonoBehaviour
 {
-    public GameObject prefab;
-    public Vector3 spawnPosition;
-    private GameObject objectRef;
+    [SerializeField] public List<RespawnableObject> objects;
+    [SerializeField] public bool waitForAll = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        foreach(RespawnableObject obj in objects)
+        {
+            if (obj.CheckNull())
+            {
+                obj.RespawnObject(transform.position);
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(objectRef == null)
+        bool empty = true;
+        // Innefficient, temp solution
+        foreach(RespawnableObject obj in objects)
         {
-            objectRef = Instantiate(prefab);
-            objectRef.transform.position = spawnPosition + transform.position;
+            if (!obj.CheckNull())
+            {
+                empty = false;
+            }
+            else
+            {
+                if (!waitForAll)
+                {
+                    obj.RespawnObject(transform.position);
+                }
+            }
+        }
+        if (empty)
+        {
+            foreach(RespawnableObject obj in objects)
+            {
+                obj.RespawnObject(transform.position);
+            }
         }
     }
 
     void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.blue;
-        Gizmos.DrawWireSphere(spawnPosition + transform.position, 0.1f);
+        foreach(RespawnableObject obj in objects)
+        {
+            Gizmos.DrawWireSphere(obj.spawnPosition + transform.position, 0.05f);
+        }
+    }
+}
+
+[Serializable]
+public class RespawnableObject
+{
+    [SerializeField] public GameObject prefab;
+    [SerializeField] public Vector3 spawnPosition;
+    [SerializeField] public Quaternion spawnRotation;
+    private GameObject objRef = null;
+
+    public bool CheckNull()
+    {
+        return objRef == null;
+    }
+
+    public void RespawnObject(Vector3 parentOffset)
+    {
+        objRef = GameObject.Instantiate(prefab);
+        objRef.transform.position = spawnPosition + parentOffset;
+        objRef.transform.rotation = spawnRotation;
     }
 }
