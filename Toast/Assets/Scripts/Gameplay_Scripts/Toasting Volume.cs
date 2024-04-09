6b42ad4a5c26538a0ad38bdc22f0f6db93475eec
Nick Kannenberg (RIT Student) <nrk5987@rit.edu>
2024-04-09 14:06:23 -0400

    Oven partial implementation

--- Toast/Assets/Scripts/Gameplay_Scripts/Toasting Volume.cs
+++ Toast/Assets/Scripts/Gameplay_Scripts/Toasting Volume.cs
@@ -12,21 +12,36 @@ public class ToastingVolume : MonoBehaviour
 {
     // Variables --------------------------
     public float toastRate = 1.0f;
+    public Dial toastingValueDial;
     public BoxCollider volume;
     public UnityEvent onToastingStart;
     public UnityEvent onToastingStop;
 
     private bool toasting;
-    private List<NewProp> toastingObjects;
+    private List<NewProp> toastingObjects = new List<NewProp>();
 
     // Functions --------------------------
     private void Update()
     {
         if (toasting)
         {
-            foreach(NewProp obj in toastingObjects)
+            foreach(NewProp prop in toastingObjects)
             {
-                obj.toastiness += toastRate * Time.deltaTime;
+                prop.toastiness += toastRate * Time.deltaTime;
+                if (prop != null)
+                {
+                    if (prop.toastiness > .15f && !prop.attributes.HasFlag(PropFlags.Toast))
+                    {
+                        prop.AddAttribute(PropFlags.Toast);
+                        ObjectiveManager.instance.UpdateObjectives(new RequirementEvent(RequirementType.CreateObject, prop.attributes, true));
+                    }
+
+                    if (prop.toastiness > .9f && !prop.attributes.HasFlag(PropFlags.Burnt))
+                    {
+                        prop.AddAttribute(PropFlags.Burnt);
+                        ObjectiveManager.instance.UpdateObjectives(new RequirementEvent(RequirementType.CreateObject, prop.attributes, true));
+                    }
+                }
             }
         }
     }
@@ -41,6 +56,35 @@ public class ToastingVolume : MonoBehaviour
     {
         toasting = false;
         onToastingStop.Invoke();
+    }
+
+    public void SetToastingValue(int toastValue)
+    {
+        toastRate = toastValue;
+        if (toastRate == 0)
+        {
+            StopToasting();
+        }
+        else
+        {
+            StartToasting();
+        }
+    }
+
+    public void CheckToastingValue()
+    {
+        if(toastingValueDial != null)
+        {
+            toastRate = toastingValueDial.dialValue;
+            if (toastRate == 0)
+            {
+                StopToasting();
+            }
+            else
+            {
+                StartToasting();
+            }
+        }
     }
 
     void OnTriggerEnter(Collider other)
