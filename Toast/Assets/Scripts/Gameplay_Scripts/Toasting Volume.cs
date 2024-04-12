a97cc575ac802fe5ea8c6e1a3b79bd0d0deee3b1
HestiaJ <jiangruni2020@163.com>
2024-04-12 08:17:16 -0400

    Build error fixed

--- Toast/Assets/Scripts/Gameplay_Scripts/Toasting Volume.cs
+++ Toast/Assets/Scripts/Gameplay_Scripts/Toasting Volume.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
-using UnityEditor.Profiling.Memory.Experimental;
 using UnityEngine;
 using UnityEngine.Events;
 
