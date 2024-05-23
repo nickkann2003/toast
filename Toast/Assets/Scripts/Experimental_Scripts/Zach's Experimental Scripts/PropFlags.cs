7b4b5f983ce032f68b107f9aca3e99b8d369eba1
Nick Kannenberg <89743982+nickkann2003@users.noreply.github.com>
2024-05-23 11:47:43 -0400

    Fixed prop flags for objectives and prefabs

--- Toast/Assets/Scripts/Experimental_Scripts/Zach's Experimental Scripts/PropFlags.cs
+++ Toast/Assets/Scripts/Experimental_Scripts/Zach's Experimental Scripts/PropFlags.cs
@@ -22,8 +22,8 @@ public enum PropFlags
     ImmuneToPickup = 1 << 10,
     ImmuneToDrag = 1 << 11,
     ImmuneToDrop = 1 << 12,
-    Bread = 1 << 28,
-    Jam = 1 << 29,
-    Knife = 1 << 30,
-    JamLid = 1 << 32
+    JamLid = 1 << 28,
+    Bread = 1 << 29,
+    Jam = 1 << 30,
+    Knife = 1 << 31
 }
