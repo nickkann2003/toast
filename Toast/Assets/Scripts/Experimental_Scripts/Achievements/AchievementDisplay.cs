5633b339b0722769018e38f20309e10377cd62d0
mdt4624 <mdt4624@rit.edu>
2024-06-03 20:02:07 -0400

    Menu Progress

---
+++ Toast/Assets/Scripts/Experimental_Scripts/Achievements/AchievementDisplay.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+
+public class AchievementDisplay : MonoBehaviour
+{
+    public Achievement associatedAchievement;
+    public string achievementName;
+    public string description;
+    public string progress;
+    [SerializeField]
+    RectTransform rectTransform;
+    [SerializeField]
+    bool isUnlocked;
+    [SerializeField]
+    bool isHiddenAchievement;
+
+    [SerializeField]
+    TextMesh nameText, descriptionText, progressText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(!isHiddenAchievement)
+        {
+            nameText.text = "???";
+            descriptionText.text = "???";
+        }
+
+        if(associatedAchievement.HasNumericGoal)
+        {
+            progressText.text = "?/?";
+        }
+        else
+        {
+            progressText.text = string.Empty;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
