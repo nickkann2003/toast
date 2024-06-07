b4542e390b4d40bd00fbad5933e43a7d2fc10f27
mdt4624 <mdt4624@rit.edu>
2024-06-07 15:17:30 -0400

    Changed achievement menu layout

--- Toast/Assets/Scripts/Experimental_Scripts/Achievements/AchievementDisplay.cs
+++ Toast/Assets/Scripts/Experimental_Scripts/Achievements/AchievementDisplay.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-
+using UnityEngine.UIElements;
+using UnityEngine.UI;
 
 public class AchievementDisplay : MonoBehaviour
 {
@@ -20,12 +21,32 @@ public class AchievementDisplay : MonoBehaviour
     [SerializeField]
     TextMeshProUGUI nameText, descriptionText, progressText;
 
+    [SerializeField]
+    Sprite lockedSprite, unlockedSprite;
+
+    UnityEngine.UI.Image displayImage;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        // Get reference to attached image 
+        displayImage = GetComponent<UnityEngine.UI.Image>();
+
+        // Make sure achievement is hooked up
         if(associatedAchievement != null)
         {
+            // Set sprite depending on unlock status
+            if(associatedAchievement.IsUnlocked)
+            {
+                displayImage.sprite = unlockedSprite;
+            }
+            else
+            {
+                displayImage.sprite = lockedSprite;
+            }
+
+            // Set text based on hidden
             if (isHiddenAchievement)
             {
                 nameText.text = "???";
@@ -37,6 +58,7 @@ public class AchievementDisplay : MonoBehaviour
                 descriptionText.text = associatedAchievement.Description;
             }
 
+            // Display goal progress if applicable
             if (associatedAchievement.HasNumericGoal)
             {
                 if(isHiddenAchievement)
