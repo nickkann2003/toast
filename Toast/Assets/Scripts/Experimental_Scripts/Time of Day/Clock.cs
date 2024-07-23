3abadcce3eceaa3d3741c37a5427c51b174f807e
mdt4624 <mdt4624@rit.edu>
2024-07-23 13:19:11 -0400

    Time being tracked properly (No more angles pls)

--- Toast/Assets/Scripts/Experimental_Scripts/Time of Day/Clock.cs
+++ Toast/Assets/Scripts/Experimental_Scripts/Time of Day/Clock.cs
@@ -70,15 +70,6 @@ public class Clock : MonoBehaviour
     {
         if(currentRange.y > 360)
         {
-            if(currentHalf == AMPM.AM)
-            {
-                currentHalf = AMPM.PM;
-            }
-            else if(currentHalf == AMPM.PM)
-            {
-                currentHalf = AMPM.PM;
-            }
-
             // Reset range
             currentRange.x = 0;
             currentRange.y = 30;
@@ -94,11 +85,11 @@ public class Clock : MonoBehaviour
         {
             if(currentTime == ClockTimes.Time0)
             {
-                SwitchHour(currentTime = ClockTimes.Time23);
+                currentTime = ClockTimes.Time23;
             }
             else
             {
-                currentTime--;
+                SwitchHour();
             }
 
             currentRange.x -= 30;
@@ -108,62 +99,18 @@ public class Clock : MonoBehaviour
         {
             if(currentTime == ClockTimes.Time23)
             {
-                SwitchHour(currentTime = ClockTimes.Time0);
+                currentTime = ClockTimes.Time0;
             }
             else
             {
-                currentTime++;
+                SwitchHour();
             }
 
             currentRange.x += 30;
             currentRange.y += 30;
         }
 
-        /*
-        if(hourlyRotation < 0)
-        {
-            hourlyRotation += 30;
-        }
-
-        // Next time reached
-        if (hourlyRotation >= 30)
-        {
-            if(currentTime == ClockTimes.Time23)
-            {
-                currentTime = ClockTimes.Time0;
-            }
-            else
-            {
-                currentTime++;
-
-                //Debug.Log((int)(hourlyRotation / 30));
-            }
-
-            hourlyRotation = hourlyRotation - 30;
-        }
-        // Rewind, previous time reached
-        else if (hourlyRotation < 0)
-        {
-            // Special Case at midnight
-            if(currentTime == ClockTimes.Time0)
-            {
-                currentTime = ClockTimes.Time23;
-            }
-            else
-            {
-                currentTime--;
-                hourlyRotation = hourlyRotation + 30;
-            }
-            
-        }
-        // Special case at midnight
-        else if(hourlyRotation < 0 && currentTime == ClockTimes.Time0)
-        {
-            
-        }
-        */
-
-      Debug.Log("The current time is " + currentTime.ToString() + " With a rotation of: " + hourlyRotation);
+        //Debug.Log("The current time is " + currentTime.ToString() + " With a rotation of: " + hourlyRotation);
     }
 
     public void ReceivedHandRotate(GameObject hand, float moveAmount)
@@ -201,47 +148,163 @@ public class Clock : MonoBehaviour
         RenderSettings.skybox.SetFloat("_Rotation", hourHand.gameObject.transform.localRotation.eulerAngles.y);
     }
 
-    void SwitchHour(ClockTimes time)
+    void SwitchHour()
     {
-        currentTime = time;
-
-        switch(hourHand.transform.rotation.y)
+        switch(hourHand.transform.localEulerAngles.y)
         {
             // 12
             case float i when i >= 0 && i < 30:
+                if(currentTime == ClockTimes.Time11)
+                {
+                    currentHalf = AMPM.PM;
+                }
+                
+                if(currentTime == ClockTimes.Time23)
+                {
+                    currentHalf = AMPM.AM;
+                }
+
+
+                if(currentHalf == AMPM.AM)
+                {
+                    currentTime = ClockTimes.Time0;
+                }
+                else
+                {
+                    currentTime = ClockTimes.Time12;
+                }
                 break;
             // 1
             case float i when i >= 30 && i < 60:
+                if (currentHalf == AMPM.AM)
+                {
+                    currentTime = ClockTimes.Time1;
+                }
+                else
+                {
+                    currentTime = ClockTimes.Time13;
+                }
                 break;
             // 2
             case float i when i >= 60 && i < 90:
+                if(currentHalf == AMPM.AM)
+                {
+                    currentTime = ClockTimes.Time2;
+                }
+                else
+                {
+                    currentTime = ClockTimes.Time14;
+                }
                 break;
             // 3
             case float i when i >= 90 && i < 120:
+                if(currentHalf == AMPM.AM)
+                {
+                    currentTime = ClockTimes.Time3;
+                }
+                else
+                {
+                    currentTime = ClockTimes.Time15;
+                }
                 break;
             // 4
             case float i when i >= 120 && i < 150:
+                if(currentHalf == AMPM.AM)
+                {
+                    currentTime = ClockTimes.Time4;
+                }
+                else
+                {
+                    currentTime = ClockTimes.Time16;
+                }
                 break;
             // 5
             case float i when i >= 150 && i < 180:
+                if(currentHalf == AMPM.AM)
+                {
+                    currentTime = ClockTimes.Time5;
+                }
+                else
+                {
+                    currentTime = ClockTimes.Time17;
+                }
                 break;
             // 6
             case float i when i >= 180 && i < 210:
+                if(currentHalf == AMPM.AM)
+                {
+                    currentTime = ClockTimes.Time6;
+                }
+                else
+                {
+                    currentTime = ClockTimes.Time18;
+                }
                 break;
             // 7
             case float i when i >= 210 && i < 240:
+                if(currentHalf == AMPM.AM)
+                {
+                    currentTime = ClockTimes.Time7;
+                }
+                else
+                {
+                    currentTime = ClockTimes.Time19;
+                }
                 break;
             // 8
             case float i when i >= 240 && i < 270:
+                if(currentHalf == AMPM.AM)
+                {
+                    currentTime = ClockTimes.Time8;
+                }
+                else
+                {
+                    currentTime = ClockTimes.Time20;
+                }
                 break;
             // 9 
             case float i when i >= 270 && i < 300:
+                if(currentHalf == AMPM.AM)
+                {
+                    currentTime = ClockTimes.Time9;
+                }
+                else
+                {
+                    currentTime = ClockTimes.Time21;
+                }
                 break;
             // 10
             case float i when i >= 300 && i < 330:
+                if(currentHalf == AMPM.AM)
+                {
+                    currentTime = ClockTimes.Time10;
+                }
+                else
+                {
+                    currentTime = ClockTimes.Time22;
+                }
                 break;
             // 11
             case float i when i >= 330 && i < 360:
+
+                if (currentTime == ClockTimes.Time0)
+                {
+                    currentHalf = AMPM.PM;
+                }
+
+                if(currentTime == ClockTimes.Time12)
+                {
+                    currentHalf = AMPM.AM;
+                }
+
+                if(currentHalf == AMPM.AM)
+                {
+                    currentTime = ClockTimes.Time11;
+                }
+                else
+                {
+                    currentTime = ClockTimes.Time23;
+                }
                 break;
             
         }
